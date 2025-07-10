299140f8232c35a8204db587011127829c1f2b54
BLUEROCKETSAM <haldersamannita@gmail.com>
2025-07-10 12:05:26 -0500

    Update build.js
    Now will fetch thumbnails for each of the blogs

--- build.js
+++ build.js
@@ -1,29 +1,49 @@
-import Parser from "rss-parser";
-import fs from "fs";
+import Parser from 'rss-parser';
+import { writeFileSync } from 'fs';
 
-const parser = new Parser();
+const parser = new Parser({
+  customFields: {
+    item: [
+      ['media:content', 'mediaContent'],
+      ['media:thumbnail', 'mediaThumbnail']
+    ]
+  }
+});
 
-(async () => {
-  const feeds = [
-    "https://www.primecenter.org/prime-blog?format=rss",
-    "https://www.primecenter.org/prime-in-the-news?format=rss"
-  ];
+const feedUrls = [
+  'https://www.primecenter.org/prime-blog/feed/',
+  'https://www.primecenter.org/prime-news/feed/'
+];
 
+const DEFAULT_IMAGE = 'https://www.primecenter.org/wp-content/uploads/2023/04/PRiME-Logo-Full-Color-Square.png';
+
+const fetchFeeds = async () => {
   let allItems = [];
 
-  for (const feedUrl of feeds) {
-    const feed = await parser.parseURL(feedUrl);
-    const items = feed.items.map(item => ({
-      title: item.title,
-      link: item.link,
-      pubDate: item.pubDate,
-      description: item.contentSnippet || "",
-      thumbnail: item.enclosure?.url || ""
-    }));
-    allItems = allItems.concat(items);
+  for (const url of feedUrls) {
+    const feed = await parser.parseURL(url);
+    const items = feed.items.map(item => {
+      const thumbnail =
+        item.enclosure?.url ||
+        item.mediaThumbnail?.url ||
+        item.mediaContent?.url ||
+        DEFAULT_IMAGE;
+
+      return {
+        title: item.title,
+        link: item.link,
+        pubDate: item.pubDate,
+        thumbnail: thumbnail,
+        description: item.contentSnippet || ''
+      };
+    });
+
+    allItems.push(...items);
   }
 
   allItems.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate));
 
-  fs.writeFileSync("docs/latest.json", JSON.stringify(allItems, null, 2));
-})();
+  writeFileSync('docs/latest.json', JSON.stringify(allItems, null, 2));
+};
+
+fetchFeeds().catch(console.error);
